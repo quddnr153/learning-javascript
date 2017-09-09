f73a6c3cd6dca258505f270d9d9e34c94eb5bbb1
Byungwook Lee <quddnr153@gmail.com>
2017-09-10 00:50:15 +0900

    [chapter-01] 1.8 commit

--- chapter-01/main.js
+++ chapter-01/main.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
 	paper.install(window);
 	paper.setup(document.getElementById('mainCanvas'));
 
-	var circle = Shape.Circle(200, 200, 50);
-	circle.fillColor = 'green';
+	var circle;
+	for (var x = 25; x < 400; x += 50) {
+		for (var y = 25; y < 400; y += 50) {
+			circle = Shape.Circle(x, y, 20)
+			circle.fillColor = 'green';
+		}
+	}
 
 	paper.view.draw();
 });
