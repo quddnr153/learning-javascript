63c3a96f9060dc42426ad29477e484bdb52a2dd0
Byungwook Lee <quddnr153@gmail.com>
2017-09-10 19:19:09 +0900

    [chapter-01] 1.9 commit
    - Apply user input (mouse down)

--- chapter-01/main.js
+++ chapter-01/main.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
 	paper.install(window);
 	paper.setup(document.getElementById('mainCanvas'));
 
-	var circle;
-	for (var x = 25; x < 400; x += 50) {
-		for (var y = 25; y < 400; y += 50) {
-			circle = Shape.Circle(x, y, 20)
-			circle.fillColor = 'green';
-		}
+	var tool = new Tool();
+
+	tool.onMouseDown = function(event) {
+		var circle = Shape.Circle(event.point.x, event.point.y, 20);
+		circle.fillColor = 'green';
 	}
 
 	paper.view.draw();
